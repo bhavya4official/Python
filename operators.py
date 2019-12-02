c12a72ff3b08cac6c80caec1514d584885a33c82
Bhavya SIngh <bhavya4official@gmail.com>
2019-12-02 14:14:40 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,8 @@
+print(10 / 3)  # Division (float)
+print(10 // 3)  # Integer output (floor)
+print(10 % 3)  # Modulus operator (remainder of division)
+print(10 ** 3)  # Exponent (power)
+
+x = 10
+x += 3  # Augmented / enhanced assignment operator
+print(x)
