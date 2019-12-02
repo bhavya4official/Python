b2769456f85543ef409750ebce695eb66c37e8b1
Bhavya SIngh <bhavya4official@gmail.com>
2019-12-02 11:53:41 +0530

    Create string_index.py
    Indexing flexibility in python.

---
+++ string_index.py
@@ -0,0 +1,8 @@
+course = 'Python course'
+print(course)
+print(course[:])  # by default start from index 0
+print(course[2:])
+print(course[-2:])
+print(course[:6])
+print(course[:-2])
+print(course[1:-1])  # start from index 1 to 1st character from the end
