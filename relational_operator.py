c722da24a154bbea4e21fce08b3af60979956398
Bhavya SIngh <bhavya4official@gmail.com>
2019-12-03 12:31:25 +0530

    Create relational_operator.py

---
+++ relational_operator.py
@@ -0,0 +1,14 @@
+name = input('Enter your name: ')
+if len(name) < 3:
+    print("Name must be at least  3 characters long.")
+elif len(name) > 20:
+    print("Name must be maximum of 20 charater long")
+else:
+    print(f"Hi {name} !")
+
+"""
+==
+<=
+>=
+!=  (Not equal to)
+"""
