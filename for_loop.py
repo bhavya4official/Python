3761a0eebc829383c1df0b0db257c95a6e34a0a8
Bhavya SIngh <bhavya4official@gmail.com>
2019-12-03 15:32:36 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,6 @@
+for item in ['Sam', 'Tommy', 3]:  # List - group of items (homogeneous/hetrogeneous)
+    print(item)
+for item in range(5):
+    print(item)
+for item in range(1, 10, 2):
+    print(item)
