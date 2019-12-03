8da7b89428793946de0bea7b7d9573ce83769dc6
Bhavya SIngh <bhavya4official@gmail.com>
2019-12-03 11:31:03 +0530

    Create if_statement.py

---
+++ if_statement.py
@@ -0,0 +1,10 @@
+is_hot = False
+is_cold = True
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water.")
+elif is_cold:               # pressed shift+tab
+    print("It's a cold day")
+    print("Wear warm clothes.")
+else:
+    print("It's a lovely day!")
