9d2ae2246ed59ae23c1cca52a268f9de9db98f61
Bhavya SIngh <bhavya4official@gmail.com>
2019-12-03 11:10:18 +0530

    Create math_function.py

---
+++ math_function.py
@@ -0,0 +1,8 @@
+import math  # Importing math module
+
+print(math.floor(1.9))  # floor method
+print(math.ceil(1.9))  # ceil method
+
+x = -2.6
+print(round(x))  # Round function
+print(abs(x))  # Absolute function (return positive value)
