951b60d335090e137d10966e31778f23888f2997
Bhavya SIngh <bhavya4official@gmail.com>
2019-12-04 17:40:48 +0530

    Update alphabet_pattern.py

--- alphabet_pattern.py
+++ alphabet_pattern.py
@@ -1,3 +1,12 @@
 pattern = [5, 2, 5, 2, 2]  # F
 for item in pattern:
     print('X' * item)
+
+print('\n');
+
+pattern = [5, 2, 2, 2, 5]  # C
+for item in pattern:
+    output = ''
+    for count in range(item):
+        output += 'X'
+    print(output)
